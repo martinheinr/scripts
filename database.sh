20625f4a29f9b20fcf944abaef29bfc82a53d4a5
Martin <martin.heinrich91@gmail.com>
2017-12-07 07:34:20 +0100

    Update database.sh

--- database.sh
+++ database.sh
@@ -1,7 +1,14 @@
-read -p "Enter the username: " username
-read -p "Enter the database: " database
+read -p "Enter the username you want to create: " username
+read -p "Enter the password for $username: " password
+read -p "Enter the database: " dbname
 read -p "Insert Table name : " tablename
-mysql -u $username -p $database<<EOF 
+echo "In the next step enter the mysql root password"
+sleep 1
+mysql -u root -p <<EOF
+create database $dbname;
+CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'localhost';
+use $dbname;
 create table $tablename(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, boxtype varchar(20), boxname varchar(20), value varchar(20));
 EOF
-echo "Thank you $username, you have created tables: $tablename "
+echo "Thank you $username, you have created $dbname database with  table: $tablename "
